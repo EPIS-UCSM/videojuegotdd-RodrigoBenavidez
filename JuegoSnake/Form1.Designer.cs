acc5dfb2636b6cc8c366572224185f903e912c62
RodrigoBenavidez <axesocash25@gmail.com>
2022-07-06 03:27:53 -0500

    Creacion de la clase Principal

---
+++ JuegoSnake/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace JuegoSnake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPuntos.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblPuntos.Location = new System.Drawing.Point(540, 384);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(51, 54);
+            this.lblPuntos.TabIndex = 1;
+            this.lblPuntos.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::JuegoSnake.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(603, 447);
+            this.Controls.Add(this.lblPuntos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MoverPieza);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPuntos;
+    }
+}
+
---
+++ JuegoSnake/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoSnake
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> Lista = new List<PictureBox>();
+        int TamanioPiezaPrincipal = 26, tiempo = 10;
+        PictureBox Comida = new PictureBox();
+        String Direccion = "right"; // Dirección del snake
+
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJuego();
+        }
+        public void IniciarJuego() {
+            tiempo = 10;
+            Direccion = "right";
+            timer1.Interval = 200;
+            lblPuntos.Text = "0";
+            Lista = new List<PictureBox>();
+            // Piezas iniciales 
+            for (int i = 2; 0 <= i; i--)
+            {
+                CrearSnake(Lista, this, (i * TamanioPiezaPrincipal) + 70, 80);
+            
+            }
+            CrearComida();
+
+        
+        }
+        public void CrearSnake(List<PictureBox> ListaPelota, Form formulario, int posicionx, int posiciony)
+        {
+
+        }
+        private void CrearComida()
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int nx = Lista[0].Location.X;
+            int ny = Lista[0].Location.Y;
+            // Colocar la pieza Principal( Cabeza de la serpiente)
+            Lista[0].Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("head" + Direccion);
+
+            for (int i = Lista.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+
+                }
+                else { 
+
+                }
+            }
+
+        }
+
+    }
+}
